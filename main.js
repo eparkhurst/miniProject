2d66ea8aeb3e99c3ec608b2cad85b963de1d41f5
eparkhurst <eli.parkhurst@gmail.com>
2015-11-10 17:59:25 -0700

    initial commit of weather app

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
   $('.submit').click(function(event){
     event.preventDefault();
     var $choice = $(".choice").val();
+    $(".weatherInfo").css("display","block")
     if($choice === "New York"){
-      // $('body').css("backgroundImage", 'url("http://www.easyoffer.net/p/new-york-wallpaper-hd-69q2.jpeg")');
-
+      $('body').css("backgroundImage", 'url("http://www.easyoffer.net/p/new-york-wallpaper-hd-69q2.jpeg")');
+        var $url = "https://api.forecast.io/forecast/c25f96b2d58ce49fa402a5c9a491229a/40.713,-74.001";
     }else if ($choice ==="San Fransisco") {
-      // $('body').css("backgroundImage", 'url("http://famebiography.net/wp-content/uploads/4212_san_francisco.jpg")');
-
+      $('body').css("backgroundImage", 'url("http://famebiography.net/wp-content/uploads/4212_san_francisco.jpg")');
+        var $url = "https://api.forecast.io/forecast/c25f96b2d58ce49fa402a5c9a491229a/37.783,-122.417";
     }else{
-      // $('body').css("backgroundImage", "url('http://wfiles.brothersoft.com/d/denver-skyline-sunset_95803-1600x1200.jpg')");
+      $('body').css("backgroundImage", "url('http://hempbeach.com/wp-content/uploads/2013/08/Dnever-at-night-hbtv-hemp-beach-tv.jpg')");
       var $url = "https://api.forecast.io/forecast/c25f96b2d58ce49fa402a5c9a491229a/39.753,-104.985";
     }
     console.log("working")
@@ -22,9 +23,10 @@ $(document).ready(function(){
       dataType:'jsonp',
       method: 'GET'
     }).done(function(data) {
-      var $info = data.daily.summary;
-      $('.summary').append($info.toString());
-      console.log($info);
+      var $temp = data.currently.apparentTemperature;
+      var $summary = data.daily.summary;
+      $('.summary').text($summary.toString());
+      console.log(data);
     });
 
 
